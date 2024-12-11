17fb77355279dd7e13cec4c327931f228fb0f70f
ig-17102k <ishan@maxscriptstechnologies.com>
2024-12-11 15:21:51 +0530

    Table for UG PG PHD Programs added

--- AMS/src/Components/Academics.tsx
+++ AMS/src/Components/Academics.tsx
@@ -1,10 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { colors } from "../Utils/Colors";
+import {
+  pgPrograms,
+  phdPrograms,
+  ugPrograms,
+} from "../Utils/Data/AcademicsData";
 
 const Academics = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-pink-200 to-purple-400 mt-12">
-      <Box sx={{ padding: 1.5, display: "flex", flexDirection: "row" }}>
+      <Box
+        sx={{
+          padding: 1.5,
+          display: "flex",
+          flexDirection: "column",
+          borderBottom: `3px solid ${colors.fontColor}`,
+        }}
+      >
         <Typography
           sx={{
             fontWeight: "bold",
@@ -16,15 +37,275 @@ const Academics = () => {
           ACADEMICS
         </Typography>
         <Typography sx={{ color: colors.fontColor }}>
-          <b>Shri Ramdeobaba College of Engineering and Management </b>(RCOEM)
-          is a premier institution founded by the{" "}
-          <b>Shri Ramdeobaba Sarvajanik Samiti</b> (SRSS) to promote excellence
-          in education. Known for its world-class infrastructure and dedicated
-          faculty, RCOEM offers cutting-edge programs in{" "}
-          <b>engineering, management, and technology</b>. With a focus on
-          innovation, values, and holistic development, it prepares students to
-          excel globally and contribute meaningfully to society.
+          All UG and PG programs are affiliated to Rashtrasant Tukadoji Maharaj
+          Nagpur University Nagpur (RTMNU) College name is listed in the list of
+          affiliated colleges RTMNU, Nagpur at serial no. 32 which can be
+          verified from the university’s website on the link{" "}
+          <b>
+            <a
+              href="https://www.nagpuruniversity.ac.in/pdf/LIST_OF_AFFILIATED_COLLEGES_311019.pdf"
+              target="_blank"
+            >
+              Click Here
+            </a>
+          </b>
         </Typography>
+      </Box>
+      <Box sx={{ padding: 1, display: "flex", flexDirection: "row", gap: 2 }}>
+        <Box sx={{ padding: 1 }}>
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              color: colors.fontColor,
+              fontSize: "1.1rem",
+              marginBottom: 0.5,
+            }}
+          >
+            UG Programs
+          </Typography>
+          <Box
+            sx={{
+              width: "10%",
+              height: "2px",
+              backgroundColor: colors.orangeColorLine,
+              marginBottom: 1,
+            }}
+          />
+          <TableContainer>
+            <Table
+              sx={{
+                minWidth: 450,
+                border: "1px solid black",
+              }}
+            >
+              <TableHead
+                sx={{
+                  backgroundColor: colors.lightGradientPink,
+                }}
+              >
+                <TableRow>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Sr.No.
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Programme Name
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Programme Duration
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ugPrograms.map((item) => (
+                  <TableRow
+                    sx={{
+                      backgroundColor:
+                        item.id % 2 === 0
+                          ? colors.lightGradientPink
+                          : colors.lighterPurpleAccordion,
+                    }}
+                    className="table-row"
+                  >
+                    <TableCell
+                      sx={{
+                        border: "1px solid black",
+                      }}
+                    >
+                      {item.id}
+                    </TableCell>
+                    <TableCell
+                      sx={{
+                        border: "1px solid black",
+                      }}
+                    >
+                      {item.program}
+                    </TableCell>
+                    <TableCell
+                      sx={{
+                        border: "1px solid black",
+                      }}
+                    >
+                      {item.duration}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        <Box sx={{ padding: 1 }}>
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              color: colors.fontColor,
+              fontSize: "1.1rem",
+              marginBottom: 0.5,
+            }}
+          >
+            PG Programs
+          </Typography>
+          <Box
+            sx={{
+              width: "10%",
+              height: "2px",
+              backgroundColor: colors.orangeColorLine,
+              marginBottom: 1,
+            }}
+          />
+          <TableContainer>
+            <Table sx={{ minWidth: 450 }}>
+              <TableHead
+                sx={{
+                  backgroundColor: colors.lightGradientPink,
+                }}
+              >
+                <TableRow>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Sr.No.
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Programme Name
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      color: colors.fontColor,
+                      fontWeight: "bold",
+                      border: "1px solid black",
+                    }}
+                  >
+                    Programme Duration
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {pgPrograms.map((item) => (
+                  <TableRow
+                    sx={{
+                      backgroundColor:
+                        item.id % 2 === 0
+                          ? colors.lightGradientPink
+                          : colors.lighterPurpleAccordion,
+                    }}
+                    className="table-row"
+                  >
+                    <TableCell sx={{ border: "1px solid black" }}>
+                      {item.id}
+                    </TableCell>
+                    <TableCell sx={{ border: "1px solid black" }}>
+                      {item.program}
+                    </TableCell>
+                    <TableCell sx={{ border: "1px solid black" }}>
+                      {item.duration}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+      <Typography align="center" padding={4} fontSize={"1.1rem"}>
+        In addition to above, the following departments of the college are
+        recognized as a place for higher learning and research (Doctoral
+        Programme) by the Rashtrasant Tukadoji Maharaj Nagpur University,
+        Nagpur- 440001
+      </Typography>
+      <Box
+        sx={{
+          paddingTop: 1,
+          paddingLeft: "5rem",
+          paddingRight: "5rem",
+          paddingBottom: 2,
+
+          display: "flex",
+        }}
+      >
+        <TableContainer>
+          <Table
+            sx={{
+              width: "50%",
+              margin: "auto",
+              border: "1px solid black",
+            }}
+          >
+            <TableHead
+              sx={{
+                backgroundColor: colors.lightGradientPink,
+              }}
+            >
+              <TableRow>
+                <TableCell
+                  sx={{
+                    color: colors.fontColor,
+                    fontWeight: "bold",
+                    border: "1px solid black",
+                  }}
+                >
+                  Sr.No.
+                </TableCell>
+                <TableCell
+                  sx={{
+                    color: colors.fontColor,
+                    fontWeight: "bold",
+                    border: "1px solid black",
+                  }}
+                >
+                  Program Name
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {phdPrograms.map((item) => (
+                <TableRow
+                  sx={{
+                    backgroundColor:
+                      item.id % 2 === 0
+                        ? colors.lightGradientPink
+                        : colors.lighterPurpleAccordion,
+                  }}
+                  className="table-row"
+                >
+                  <TableCell sx={{ border: "1px solid black" }}>
+                    {item.id}
+                  </TableCell>
+                  <TableCell sx={{ border: "1px solid black" }}>
+                    {item.program}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </div>
   );
---
+++ AMS/src/Utils/Data/AcademicsData.tsx
@@ -0,0 +1,102 @@
+export const ugPrograms = [
+  { id: 1, program: "B.E. Biomedical Engineering", duration: "4 years" },
+  { id: 2, program: "B.E. Civil Engineering", duration: "4 years" },
+  {
+    id: 3,
+    program: "B.E. Civil Engineering (Second Shift)",
+    duration: "4 years",
+  },
+  { id: 4, program: "B.E. Industrial Engineering", duration: "4 years" },
+  { id: 5, program: "B.E. Electrical Engineering", duration: "4 years" },
+  { id: 6, program: "B.E. Electronics Engineering", duration: "4 years" },
+  {
+    id: 7,
+    program: "B.E. Electronics Engineering (Second Shift)",
+    duration: "4 years",
+  },
+  { id: 8, program: "B.E. Electronics Design Technology", duration: "4 years" },
+  { id: 9, program: "B.E. Electronics and Communication", duration: "4 years" },
+  {
+    id: 10,
+    program: "B.E. Electronics and Communication (Second Shift)",
+    duration: "4 years",
+  },
+  { id: 11, program: "B.E. Information Technology", duration: "4 years" },
+  {
+    id: 12,
+    program: "B.E. Computer Science and Engineering",
+    duration: "4 years",
+  },
+  {
+    id: 13,
+    program:
+      "B.E. Computer Science and Engineering (Artificial Intelligence and Machine Learning)",
+    duration: "4 years",
+  },
+  {
+    id: 14,
+    program: "B.E. Computer Science and Engineering (Data Science)",
+    duration: "4 years",
+  },
+  {
+    id: 15,
+    program: "B.E. Computer Science and Engineering (Cyber Security)",
+    duration: "4 years",
+  },
+  {
+    id: 16,
+    program: "B.E. Computer Science and Engineering (Second Shift)",
+    duration: "4 years",
+  },
+  { id: 17, program: "B.E. Mechanical Engineering", duration: "4 years" },
+];
+
+export const pgPrograms = [
+  { id: 1, program: "M.Tech Robotics and Automation", duration: "2 years" },
+  { id: 2, program: "M.Tech Structural Engineering", duration: "2 years" },
+  { id: 3, program: "M.Tech Industrial Engineering", duration: "2 years" },
+  { id: 4, program: "M.Tech VLSI Design", duration: "2 years" },
+  {
+    id: 5,
+    program: "M.Tech Geo-Technical Engineering (Part Time)",
+    duration: "2.5 years",
+  },
+  {
+    id: 6,
+    program: "M.Tech Computer Science and Engineering",
+    duration: "2 years",
+  },
+  {
+    id: 7,
+    program:
+      "M.Tech Electrical Engineering (Power Electronics and Power Systems)",
+    duration: "2 years",
+  },
+  { id: 8, program: "M.Tech Heat Power Engineering", duration: "2 years" },
+  { id: 9, program: "Master of Computer Applications", duration: "2 years" },
+  {
+    id: 10,
+    program: "Master of Computer Applications (Second Shift)",
+    duration: "2 years",
+  },
+  { id: 11, program: "Master of Business Administration", duration: "2 years" },
+  {
+    id: 12,
+    program: "Master of Business Administration (Second Shift)",
+    duration: "2 years",
+  },
+  {
+    id: 13,
+    program: "Master of Business Administration (Integrated)",
+    duration: "5 years",
+  },
+];
+
+export const phdPrograms = [
+  { id: 1, program: "Civil Engineering" },
+  { id: 2, program: "Computer Science & Engineering" },
+  { id: 3, program: "Electrical Engineering" },
+  { id: 4, program: "Electronics Engineering" },
+  { id: 5, program: "Mechanical Engineering" },
+  { id: 6, program: "Business Administration and Business Management" },
+];
